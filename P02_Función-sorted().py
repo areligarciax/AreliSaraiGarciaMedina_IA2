ce7f0f01d71b2be44d36f167b106d38126e01eb2
areligarciax <areligmx@outlook.com>
2023-03-26 17:51:32 -0600

    Práctica 2
    Función sorted()

---
+++ P02_Función-sorted().py
@@ -0,0 +1,13 @@
+# Areli Sarai García Medina | 20310380
+
+lista = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+
+# Ordenar la lista en orden ascendente
+nueva_lista = sorted(lista)
+print("Lista nueva en orden ascendente:")
+print(nueva_lista)
+
+# Ordenar la lista en orden descendente
+nueva_lista = sorted(lista, reverse=True)
+print("\nLista nueva en orden descendente:")
+print(nueva_lista)
