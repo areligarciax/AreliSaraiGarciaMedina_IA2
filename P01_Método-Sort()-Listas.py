7f0c515bdae3b41ffa999a0b678f01f4cfc4e1c4
areligarciax <areligmx@outlook.com>
2023-03-26 17:49:22 -0600

    Práctica 1

---
+++ P01_Método-Sort()-Listas.py
@@ -0,0 +1,13 @@
+# Areli Sarai García Medina | 20310380
+
+lista = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+
+# Ordenar la lista en orden ascendente
+lista.sort()
+print("Lista en orden ascendente:")
+print(lista)
+
+# Ordenar la lista en orden descendente
+lista.sort(reverse=True)
+print("\nLista en orden descendente:")
+print(lista)
