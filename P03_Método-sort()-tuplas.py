8e114086e9cd41fe5b121fa9528f40001a2cd7f8
areligarciax <areligmx@outlook.com>
2023-03-26 17:53:37 -0600

    Práctica 3
    Método sort() y tuplas

---
+++ P03_Método-sort()-tuplas.py
@@ -0,0 +1,7 @@
+# Areli Sarai García Medina | 20310380
+
+tupla = (3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)
+
+# Convertir la tupla en una lista, ordenarla y convertirla de nuevo en una tupla
+nueva_tupla = tuple(sorted(list(tupla)))
+print(nueva_tupla)
