91a39d5e154730cf87e160bc01f9be6c3da15d37
areligarciax <areligmx@outlook.com>
2023-03-26 18:01:27 -0600

    Práctica 6
    Función sorted() con la opción reverse

---
+++ P06_Función-sorted()-opción-reverse.py
@@ -0,0 +1,17 @@
+# Areli Sarai García Medina | 20310380
+
+lista = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+
+# Ordenar la lista en orden descendente
+nueva_lista = sorted(lista, reverse=True)
+
+# Imprimir la nueva lista en orden descendente
+print("Lista en orden descendente:")
+print(nueva_lista)
+
+# Ordenar la lista en orden ascendente
+nueva_lista2 = sorted(lista, reverse=False)
+
+# Imprimir la nueva lista en orden ascendente
+print("\nLista en orden ascendente:")
+print(nueva_lista2)
